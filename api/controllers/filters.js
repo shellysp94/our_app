6ec8c905ae8847ed4f7ddcd8ecd8dd149c4ab771
MorZemach <90982428+MorZemach@users.noreply.github.com>
2022-05-05 20:31:45 +0300

    remove console.log from filters

--- api/controllers/filters.js
+++ api/controllers/filters.js
@@ -260,9 +260,6 @@ getUserFilteredUsers = (req, res) => {
 	let interestingIn = [];
 	let age = [];
 
-	// console.log(typeof req.body.age_filter);
-	// console.log(req.body.age_filter);
-
 	getUserFilter(req, (userFilter) => {
 		//console.log(userFilter[0]);
 		if (userFilter.length === 0) {
@@ -276,7 +273,7 @@ getUserFilteredUsers = (req, res) => {
 					usersToSend.push(user.user_id);
 				});
 
-				console.log(usersToSend);
+				//console.log(usersToSend);
 				let resultArrayToObject = {
 					params: {userid: String(usersToSend)},
 				};
@@ -560,128 +557,6 @@ deleteUserFilter = (req, res) => {
 		}
 	);
 };
-
-// returnAllFiltersInSet = (req, res) => {
-// 	let filters = {
-// 		Hobbies: [
-// 			{
-// 				type: "Default",
-// 				value: "Hobbies",
-// 			},
-// 			{
-// 				type: "Sport",
-// 				lst: [
-// 					"basketball",
-// 					"beach volleyball",
-// 					"crossfit",
-// 					"dancing",
-// 					"football/soccer",
-// 					"gym workout",
-// 					"hiking",
-// 					"pilates",
-// 					"running",
-// 					"slacklining",
-// 					"surfing",
-// 					"swimming",
-// 					"tennis",
-// 					"yoga",
-// 				],
-// 			},
-// 			{
-// 				type: "Food",
-// 				lst: [
-// 					"baking",
-// 					"cooking",
-// 					"eating outside",
-// 					"interested in culinary",
-// 					"interesting in nutrition",
-// 				],
-// 			},
-// 			{
-// 				type: "Music",
-// 				lst: [
-// 					"playing drums",
-// 					"playing guitar",
-// 					"playing in a band",
-// 					"playing piano",
-// 					"playing synthesizer",
-// 					"singing",
-// 				],
-// 			},
-// 			{
-// 				type: "Art",
-// 				lst: [
-// 					"acting",
-// 					"fashion designing",
-// 					"handicraft",
-// 					"home decorating",
-// 					"juggling",
-// 					"painting",
-// 				],
-// 			},
-// 			{
-// 				type: "Knowledge",
-// 				lst: [
-// 					"blogging",
-// 					"interested in medicine and biology",
-// 					"learning new languages",
-// 					"listening to podcasts",
-// 					"playing chess",
-// 					"puzzling",
-// 					"reading",
-// 					"writing",
-// 				],
-// 			},
-// 			{
-// 				type: "Technology",
-// 				lst: ["coding", "hacking", "playing video games"],
-// 			},
-// 			{
-// 				type: "Outside_Inside",
-// 				lst: [
-// 					"camping",
-// 					"gardening",
-// 					"sailing",
-// 					"skippering",
-// 					"shopping",
-// 					"tanning",
-// 					"traveling",
-// 				],
-// 			},
-// 		],
-// 		Search_Mode: [
-// 			"Whatever",
-// 			"Beer",
-// 			"Study",
-// 			"Food",
-// 			"Training",
-// 			"Coffee",
-// 			"Shopping",
-// 		],
-// 		Gender: ["Gender", "Men", "Women", "All"],
-// 		Relationship: [
-// 			"Relationship",
-// 			"Divorced",
-// 			"Engaged",
-// 			"In a relationship",
-// 			"In an open relationship",
-// 			"Married",
-// 			"Single",
-// 			"Widowed",
-// 		],
-// 		Interesting_In: [
-// 			"Interesting in",
-// 			"Friends",
-// 			"Hookup",
-// 			"Long term relationship",
-// 			"Short term relationship",
-// 			"Sport buddy",
-// 			"Study buddy",
-// 			"Work buddy",
-// 		],
-// 	};
-// 	return res.send(filters);
-// };
 
 module.exports = {
 	getAllFilters: getAllFilters,
