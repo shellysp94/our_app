4ffb8fdcd380eb7d8638b641e2fa7ab9c129231b
shellysp94 <59768741+shellysp94@users.noreply.github.com>
2022-05-06 15:03:53 +0300

    Add gitignore, untrack node_modules

--- api/routes/dataFromSetsToClient.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {returnAllFiltersInSet} = require("../controllers/dataFromSetsToClient");
-
-router.get("/", returnAllFiltersInSet);
-
-module.exports = router;
--- config/auth_config.js
+++
@@ -1,3 +0,0 @@
-const accessTokenSecret = 'b989679885c6f3e227';
-
-module.exports = accessTokenSecret
