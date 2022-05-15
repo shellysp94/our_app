127b898070518432d4750557f4c2abeb017e5c9e
MorZemach <90982428+MorZemach@users.noreply.github.com>
2022-05-15 16:41:59 +0300

    messages API

--- api/controllers/messages.js
+++ api/controllers/messages.js
@@ -5,8 +5,6 @@ getChatMessages = (req, res) => {
 	const chatID = req.params.chatID;
 	const messagesOffset = req.params.messagesOffset;
 
-	console.log("checking for git");
-
 	mySqlConnection.query(
 		`select * from messages where chat_id = ${chatID} order by create_date asc limit 50 offset ${messagesOffset}`,
 		(err, rows) => {
@@ -20,16 +18,19 @@ getChatMessages = (req, res) => {
 };
 
 createChatMessage = (req, res) => {
-	const chatID = req.param.chatID;
-	const sender = req.param.sender;
-	const receiver = req.param.receiver;
+	const chatID = req.params.chatID;
+	const sender = req.params.sender;
+	const receiver = req.params.receiver;
 	const content = req.body.content;
 
 	mySqlConnection.query(
-		`insert into messages (chat_id, create_date, sender_user_id, receiver_user_id, content) values (${chatID}, curdate(), ${sender}, ${receiver}, ${content})`,
+		`insert into messages (chat_id, create_date, sender_user_id, receiver_user_id, content) values (${chatID}, curdate(), ${sender}, ${receiver}, "${content}")`,
 		(err, rows) => {
 			try {
-				res.send(rows);
+				msgToClient = {
+					msg: `message for chat room number: ${chatID} created`,
+				};
+				return res.send(msgToClient);
 			} catch (err) {
 				console.log(err.message);
 			}
@@ -37,7 +38,21 @@ createChatMessage = (req, res) => {
 	);
 };
 
+deleteChatMessages = (req, res) => {
+	const chatID = req.params.chatID;
+	mySqlConnection.query(
+		`delete from messages where chat_id = ${chatID}`,
+		(err, rows) => {
+			msgToClient = {
+				msg: `Chat room number ${chatID} is empty`,
+			};
+			return res.send(msgToClient);
+		}
+	);
+};
+
 module.exports = {
 	getChatMessages,
 	createChatMessage,
+	deleteChatMessages,
 };
--- api/routes/messages.js
+++ api/routes/messages.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {getChatMessages, createChatMessage} = require("../controllers/messages");
+const {
+	getChatMessages,
+	createChatMessage,
+	deleteChatMessages,
+} = require("../controllers/messages");
 
 router.get("/:chatID/:messagesOffset", getChatMessages);
 router.post("/:chatID/:sender/:receiver", createChatMessage);
+router.delete("/:chatID", deleteChatMessages);
 
 module.exports = router;
