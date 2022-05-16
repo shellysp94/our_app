4ddcf7c4a104135e935d91e4b4c52c2154c1f1e2
shellysp94 <59768741+shellysp94@users.noreply.github.com>
2022-05-16 12:45:09 +0300

    add verify token to gen configuration

--- api/routes/userConfiguration.js
+++ api/routes/userConfiguration.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {verifyToken} = require('../controllers/auth');
 const router = express.Router();
 
 const {
@@ -10,7 +11,7 @@ const {
     updateSearchMode
 } = require('../controllers/userConfiguration');
 
-router.get('/:userid',getUserConfiguration); //get user persaonal details by appid (/PersonalDetails/:appid)
+router.get('/:userid',verifyToken,getUserConfiguration); //get user persaonal details by appid (/PersonalDetails/:appid)
 router.get('/',getAllUsersConfiguration);
 router.post('/:userid', createUserConfiguration); //post (/PersonalDetails/)
 router.delete('/:userid', deleteUserConfiguration); //delete user persaonal details (/PersonalDetails/:appid)
