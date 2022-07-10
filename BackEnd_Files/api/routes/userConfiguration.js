ae52933fa35576a555932056df9fa186e4b2c1ac
MorZemach <90982428+MorZemach@users.noreply.github.com>
2022-07-10 14:22:06 +0300

    update npm start path

--- BackEnd_Files/api/routes/userConfiguration.js
+++ BackEnd_Files/api/routes/userConfiguration.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const {verifyToken} = require('../controllers/auth');
+const express = require("express");
+const {verifyToken} = require("../controllers/auth");
 const router = express.Router();
 
 const {
-    getAllUsersConfiguration,
-    getUserConfiguration, 
-    createUserConfiguration,
-    deleteUserConfiguration,
-    updateUserConfiguration,
-    updateSearchMode
-} = require('../controllers/userConfiguration');
+	getAllUsersConfiguration,
+	getUserConfiguration,
+	createUserConfiguration,
+	deleteUserConfiguration,
+	updateUserConfiguration,
+	updateSearchMode,
+} = require("../controllers/userConfiguration");
 
-router.get('/:userid',getUserConfiguration); //get user persaonal details by appid (/PersonalDetails/:appid)
-router.get('/',getAllUsersConfiguration);
-router.post('/:userid', createUserConfiguration); //post (/PersonalDetails/)
-router.delete('/:userid', deleteUserConfiguration); //delete user persaonal details (/PersonalDetails/:appid)
-router.put('/:userid', updateUserConfiguration);
-router.put('/searchMode/:userid', updateSearchMode)
+router.get("/:userid", getUserConfiguration); //get user personal details by appid (/PersonalDetails/:appid)
+router.get("/", getAllUsersConfiguration);
+router.post("/:userid", createUserConfiguration); //post (/PersonalDetails/)
+router.delete("/:userid", deleteUserConfiguration); //delete user personal details (/PersonalDetails/:appid)
+router.put("/:userid", updateUserConfiguration);
+router.put("/searchMode/:userid", updateSearchMode);
 
-module.exports=router;
+module.exports = router;
