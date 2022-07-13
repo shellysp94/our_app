5000d2bca6729f902689bd9bbe2991f4a1a75c00
MorZemach <90982428+MorZemach@users.noreply.github.com>
2022-07-13 21:32:21 +0300

    A new query for 'get chats of a specific user' by user id - return users cinfigurations + last message of the chat room if there is. update the sort of the return messages for the client

--- BackEnd_Files/api/routes/chats.js
+++ BackEnd_Files/api/routes/chats.js
@@ -3,14 +3,16 @@ const router = express.Router();
 
 const {
 	getAllChats,
+	getSpecificUserChats,
 	getUsersChat,
 	createUsersChat,
 	deleteUsersChat,
 } = require("../controllers/chats");
 
-router.get("/", getAllChats); //get all chats (/chats)
-router.get("/:useridA/:useridB/:offset", getUsersChat); //get a chat by A_userid and B_userid (/chats/:useridA/:useridB)
-router.post("/:useridA/:useridB", createUsersChat); //post a chat (/chats/:useridA/:useridB)
-router.delete("/:useridA/:useridB", deleteUsersChat); //delete a chat (/chats/:useridA/:useridB)
+router.get("/", getAllChats);
+router.get("/:userid", getSpecificUserChats);
+router.get("/:useridA/:useridB/:offset", getUsersChat);
+router.post("/:useridA/:useridB", createUsersChat);
+router.delete("/:useridA/:useridB", deleteUsersChat);
 
 module.exports = router;
