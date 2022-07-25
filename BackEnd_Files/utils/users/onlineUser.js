6b2a70aa3a27e3a11002dedc06c96c764e22f854
MorZemach <90982428+MorZemach@users.noreply.github.com>
2022-07-25 16:43:16 +0300

    fixed the WS connection and online users array (async)

--- BackEnd_Files/utils/users/onlineUser.js
+++ BackEnd_Files/utils/users/onlineUser.js
@@ -3,34 +3,80 @@ const chatRooms = new chatRoomsArray().getInstance().getChatRoomsArray();
 const dbConfig = require("../../config/db_config");
 const mySqlConnection = dbConfig;
 
-function getUserName(user_id, callback) {
-	mySqlConnection.query(
-		`select first_name from user_configuration where user_id = ${user_id}`,
-		(err, rows) => {
-			if (typeof rows === "undefined" || rows.length === 0) {
-				console.log(
-					"something wrong. This user id didn't exist in the database"
-				);
-				return callback();
-			} else {
-				return callback(rows[0].first_name);
+function getUserName(user_id) {
+	return new Promise((resolve, reject) => {
+		mySqlConnection.query(
+			`select first_name from user_configuration where user_id = ${user_id}`,
+			(err, rows) => {
+				if (err) {
+					reject(err);
+				}
+				if (rows === undefined || rows.length === 0) {
+					console.log(
+						"something wrong. This user id didn't exist in the database"
+					);
+					resolve("");
+				} else {
+					resolve(rows[0].first_name);
+				}
 			}
-		}
-	);
+		);
+	});
+	// mySqlConnection.query(
+	// 	`select first_name from user_configuration where user_id = ${user_id}`,
+	// 	(err, rows) => {
+	// 		if (typeof rows === "undefined" || rows.length === 0) {
+	// 			console.log(
+	// 				"something wrong. This user id didn't exist in the database"
+	// 			);
+	// 			return callback();
+	// 		} else {
+	// 			return callback(rows[0].first_name);
+	// 		}
+	// 	}
+	// );
 }
 
 let onlineUser = class {
-	constructor(user_id, websocket) {
-		getUserName(user_id, (userName) => {
-			this.user_id = user_id;
-			this.user_name = userName;
-			this.websocket = websocket;
-			this.userChatRooms = chatRooms.filter(
-				(chatRoom) =>
-					chatRoom.getUser_A_id() === parseInt(user_id, 10) ||
-					chatRoom.getUser_B_id() === parseInt(user_id, 10)
-			);
-		});
+	constructor() {
+		this.user_id;
+		this.user_name;
+		this.websocket;
+		this.userChatRooms;
+		console.log("C'tor of online user");
+		// getUserName(user_id, (userName) => {
+		// 	this.user_id = user_id;
+		// 	this.user_name = userName;
+		// 	this.websocket = websocket;
+		// 	this.userChatRooms = chatRooms.filter(
+		// 		(chatRoom) =>
+		// 			chatRoom.getUser_A_id() === parseInt(user_id, 10) ||
+		// 			chatRoom.getUser_B_id() === parseInt(user_id, 10)
+		// 	);
+		// 	console.log(`C'tor of user: ${user_id} create an online user!`);
+		// console.log(
+		// 	`Online User C'tor --> create online user with id: ${user_id}.\nThe object is: ${JSON.stringify(
+		// 		this
+		// 	)}`
+		// );
+		//});
+	}
+
+	async initOnlineUser(user_id, websocket) {
+		const userName = await getUserName(user_id);
+		this.user_id = user_id;
+		this.user_name = userName;
+		this.websocket = websocket;
+		this.userChatRooms = chatRooms.filter(
+			(chatRoom) =>
+				chatRoom.getUser_A_id() === parseInt(user_id, 10) ||
+				chatRoom.getUser_B_id() === parseInt(user_id, 10)
+		);
+		console.log(`C'tor of user: ${user_id} create an online user!`);
+	}
+
+	updateOnlineUserChatRooms() {
+		console.log(`Update chat rooms array of user: ${this.user_id}`);
 	}
 
 	getUserId() {
--- BackEnd_Files/utils/users/onlineUsersArray.js
+++ BackEnd_Files/utils/users/onlineUsersArray.js
@@ -3,19 +3,47 @@ const onlineUser = require("./onlineUser");
 let onlineUsersArray = class {
 	constructor() {
 		this.onlineUsers = [];
+
+		console.log("Online users array ctor:");
 	}
 
-	insertNewOnlineUser(user_id, websocket) {
-		const newOnlineUser = new onlineUser(user_id, websocket);
-		if (!this.onlineUsers.includes(newOnlineUser)) {
+	async insertNewOnlineUser(user_id, websocket) {
+		let isExists = 0;
+		this.onlineUsers.forEach((onlineUser) => {
+			if (parseInt(onlineUser.getUserId()) === parseInt(user_id)) {
+				isExists = 1;
+			}
+		});
+
+		if (isExists === 0) {
+			const newOnlineUser = new onlineUser();
+			await newOnlineUser.initOnlineUser(user_id, websocket);
 			this.onlineUsers.push(newOnlineUser);
+			console.log(
+				`Insert a new online user-\nInserting user_id = ${user_id} and his WS to the array!`
+			);
+		} else {
+			console.log(
+				`Insert a new online user-\nUser id ${user_id} is already in the array. Didn't insert him again.`
+			);
 		}
+
+		//console.log(`Online users array\n${JSON.stringify(this)}`);
 	}
 
 	removeAnOnlineUser(user_id) {
 		this.onlineUsers.forEach((onlineUser) => {
 			if (onlineUser.getUserId() === user_id) {
-				this.onlineUsers.splice(this.onlineUsers.indexOf(onlineUser), 1);
+				console.log("The ready state of ws:", onlineUser.websocket.readyState);
+				if (
+					onlineUser.websocket.readyState === 2 ||
+					onlineUser.websocket.readyState === 3
+				) {
+					this.onlineUsers.splice(this.onlineUsers.indexOf(onlineUser), 1);
+					console.log(
+						"Remove from online users array:\n" + JSON.stringify(this)
+					);
+				}
 			}
 		});
 	}
@@ -25,9 +53,15 @@ let onlineUsersArray = class {
 	}
 
 	getOnlineUser(user_id) {
-		return this.onlineUsers.find(
-			(user) => user.user_id === parseInt(user_id, 10)
+		let onlineUser = this.onlineUsers.find(
+			(user) => parseInt(user.user_id) === parseInt(user_id)
 		);
+
+		console.log(
+			`Get an online user with user_id = ${user_id} (from the array class):\n` +
+				onlineUser
+		);
+		return onlineUser;
 	}
 };
 
