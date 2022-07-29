b7d86459f4107dd4b0450d3560160f5a80ba7c74
shellysp94 <59768741+shellysp94@users.noreply.github.com>
2022-07-29 17:39:57 +0300

    async in websocket - fixed

--- BackEnd_Files/server.js
+++ BackEnd_Files/server.js
@@ -12,39 +12,107 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 const onlineUsers = new onlineUsersArray().getInstance();
 
-wss.on("connection", (ws, req) => {
-	const token = req.headers.authorization.split(" ")[1];
-	//let token = url.parse(req.url, true).query.token;
-	let userid;
+const myQuery = (query) =>
+{
+    return new Promise((resolve, reject) =>
+    {
+        mySqlConnection.query(query,(err,rows) =>
+        {
+            if(err)
+            {
+                reject(err);
+            }
+            resolve(rows);
+        });
+    });
+}
 
-	mySqlConnection.query(
-		`select user_id from users where token = "${token}"`,
-		(err, rows) => {
-			try {
-				if (rows.length > 0) {
-					userid = rows[0].user_id;
-					jwt.verify(token, publicToken, async (err, payload) => {
-						if (err) {
-							console.log(`Error: ${err.message}. connection closed`);
-							ws.close();
-						} else {
-							await onlineUsers.insertNewOnlineUser(userid, ws);
-							console.log("--------------------------------------------");
-							console.log("Online users array is now:\n");
-							console.log("--------------------------------------------");
-							console.log(onlineUsers);
-							//console.log(`user number ${userid} connected`);
-						}
-					});
-				} else {
-					console.log("Token illegal. connection closed");
-					ws.close();
-				}
-			} catch (err) {
-				console.log(err.message);
-			}
-		}
-	);
+// wss.on("connection", (ws, req) => {
+// 	//const token = req.headers.authorization.split(" ")[1];
+// 	let token = url.parse(req.url, true).query.token;
+// 	let userid;
+
+// 	mySqlConnection.query(
+// 		`select user_id from users where token = "${token}"`,
+// 		(err, rows) => {
+// 			try {
+// 				if (rows.length > 0) {
+// 					userid = rows[0].user_id;
+// 					jwt.verify(token, publicToken, async (err, payload) => {
+// 						if (err) {
+// 							console.log(`Error: ${err.message}. connection closed`);
+// 							ws.close();
+// 						} else {
+// 							await onlineUsers.insertNewOnlineUser(userid, ws);
+// 							console.log("--------------------------------------------");
+// 							console.log("Online users array is now:\n");
+// 							console.log("--------------------------------------------");
+// 							console.log(onlineUsers);
+// 							//console.log(`user number ${userid} connected`);
+// 						}
+// 					});
+// 				} else {
+// 					console.log("Token illegal. connection closed");
+// 					ws.close();
+// 				}
+// 			} catch (err) {
+// 				console.log(err.message);
+// 			}
+// 		}
+// 	);
+
+//     ws.on('message',message =>
+//     {
+//         console.log(message);
+//     })
+// 	ws.on("close", () => {
+// 		onlineUsers.removeAnOnlineUser(userid);
+// 		console.log(`user number ${userid} disconnected`);
+// 	});
+// });
+
+wss.on("connection", async(ws, req) => {
+    try
+    {
+		let token = url.parse(req.url, true).query.token;
+        //const token = req.headers.authorization.split(" ")[1];
+        let userid;
+        const rows = await myQuery(`select user_id from users where token = "${token}"`);
+        if (rows.length > 0) 
+        {
+            userid = rows[0].user_id;
+            jwt.verify(token, publicToken, async (err, payload) => 
+            {
+                if (err) 
+                {
+                    console.log(`Error: ${err.message}. connection closed`);
+                    ws.close();
+                } 
+                else 
+                {
+                    await onlineUsers.insertNewOnlineUser(userid, ws);
+                    console.log("--------------------------------------------");
+                    console.log("Online users array is now:\n");
+                    console.log("--------------------------------------------");
+                    console.log(onlineUsers);
+                }
+            });
+        }
+        else
+        {
+            console.log("Token illegal. connection closed");
+			ws.close();
+        }       
+    }
+    catch (e)
+    {
+        console.log(e.message);
+    }
+
+	ws.on("message", message =>
+	{
+		console.log(`Recieved message => ${message} `)
+	})
 
 	ws.on("close", () => {
 		onlineUsers.removeAnOnlineUser(userid);
