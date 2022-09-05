8524565bd81662c3c8d73db35d1d84171185fd1c
MorZemach <90982428+MorZemach@users.noreply.github.com>
2022-09-05 16:52:41 +0300

    messages + chats - QA

--- BackEnd_Files/api/controllers/chats.js
+++ BackEnd_Files/api/controllers/chats.js
@@ -12,9 +12,14 @@ getAllChats = (req, res) => {
 	logger.info("This is an info log");
 	mySqlConnection.query("SELECT * from Chats", (err, rows) => {
 		try {
-			res.send(rows);
+			if (err) {
+				throw new Error("Chat - GET MySQL Error");
+			} else {
+				res.send(rows);
+			}
 		} catch (err) {
-			console.log(err.message);
+			logger.error("Chats", {err});
+			return res.status(500).send("internal error");
 		}
 	});
 };
@@ -28,81 +33,91 @@ getSpecificUserChats = (req, res) => {
 		`select user_A_id, user_B_id from Chats where user_A_id = ${userid} or user_B_id = ${userid}`,
 		(err, rows) => {
 			try {
-				if (rows !== undefined && rows.length > 0) {
-					let openChatUsers = [];
-					for (let user = 0; user < rows.length; user++) {
-						if (parseInt(rows[user].user_a_id, 10) !== parseInt(userid, 10)) {
-							openChatUsers.push(parseInt(rows[user].user_a_id, 10));
-						} else {
-							openChatUsers.push(parseInt(rows[user].user_b_id, 10));
+				if (err || rows === undefined) {
+					throw new Error(
+						"Chats - GET (specific user's chat rooms) MySQL Error"
+					);
+				} else {
+					if (rows.length > 0) {
+						let openChatUsers = [];
+						for (let user = 0; user < rows.length; user++) {
+							if (parseInt(rows[user].user_a_id, 10) !== parseInt(userid, 10)) {
+								openChatUsers.push(parseInt(rows[user].user_a_id, 10));
+							} else {
+								openChatUsers.push(parseInt(rows[user].user_b_id, 10));
+							}
 						}
-					}
-					let resultArrayToObject = {
-						params: {userid: String(openChatUsers)},
-					};
-					getUserConfigurationInner(
-						resultArrayToObject,
-						userid,
-						(relevantUsersConfiguration) => {
-							// get the last message in a specific chat to display
-							for (
-								let relevant = 0;
-								relevant < relevantUsersConfiguration.length;
-								relevant++
-							) {
-								mySqlConnection.query(
-									`select * from Messages where
+						let resultArrayToObject = {
+							params: {userid: String(openChatUsers)},
+						};
+						getUserConfigurationInner(
+							resultArrayToObject,
+							userid,
+							(relevantUsersConfiguration) => {
+								// get the last message in a specific chat to display
+								for (
+									let relevant = 0;
+									relevant < relevantUsersConfiguration.length;
+									relevant++
+								) {
+									mySqlConnection.query(
+										`select * from Messages where
 								(sender_user_id = ${userid} and receiver_user_id = ${relevantUsersConfiguration[relevant].user_id}) or
 								(sender_user_id = ${relevantUsersConfiguration[relevant].user_id} and receiver_user_id = ${userid})
 								order by creation_date desc limit 1`,
-									(err, rows) => {
-										try {
-											if (rows !== undefined && rows.length > 0) {
-												for (user = 0; user < rows.length; user++) {
-													if (
-														parseInt(
-															relevantUsersConfiguration[relevant].user_id,
-															10
-														) === parseInt(rows[user].sender_user_id, 10) ||
-														parseInt(
-															relevantUsersConfiguration[relevant].user_id,
-															10
-														) === parseInt(rows[user].receiver_user_id, 10)
-													) {
-														// merging
-														mergeObjects.push(
-															Object.assign(
-																{},
-																relevantUsersConfiguration[relevant],
-																rows[user]
-															)
-														);
+										(err, rows) => {
+											try {
+												if (rows !== undefined && rows.length > 0) {
+													for (user = 0; user < rows.length; user++) {
+														if (
+															parseInt(
+																relevantUsersConfiguration[relevant].user_id,
+																10
+															) === parseInt(rows[user].sender_user_id, 10) ||
+															parseInt(
+																relevantUsersConfiguration[relevant].user_id,
+																10
+															) === parseInt(rows[user].receiver_user_id, 10)
+														) {
+															// merging
+															mergeObjects.push(
+																Object.assign(
+																	{},
+																	relevantUsersConfiguration[relevant],
+																	rows[user]
+																)
+															);
+														}
 													}
+												} else {
+													mergeObjects.push(
+														Object.assign(
+															{},
+															relevantUsersConfiguration[relevant]
+														)
+													);
 												}
-											} else {
-												mergeObjects.push(
-													Object.assign(
-														{},
-														relevantUsersConfiguration[relevant]
-													)
-												);
+												if (
+													relevant + 1 ===
+													relevantUsersConfiguration.length
+												) {
+													res.send(mergeObjects);
+												}
+											} catch (err) {
+												console.log(err.message);
 											}
-											if (relevant + 1 === relevantUsersConfiguration.length) {
-												res.send(mergeObjects);
-											}
-										} catch (err) {
-											console.log(err.message);
 										}
-									}
-								);
+									);
+								}
 							}
-						}
-					);
-				} else {
-					return res.send(rows);
+						);
+					} else {
+						res.send(rows);
+					}
 				}
 			} catch (err) {
-				console.log(err.message);
+				logger.error({err});
+				return res.status(500).send("internal error");
 			}
 		}
 	);
@@ -119,23 +134,33 @@ getUsersChat = (req, res) => {
 		[userIdA, userIdB, userIdB, userIdA],
 		(err, rows) => {
 			try {
-				if (rows.length === 0) {
-					return res.send(rows);
+				if (err || rows === undefined) {
+					throw new Error("Chats - GET from Chats - MySQL Error");
 				} else {
-					const chatID = rows[0].chat_id;
-					let desiredChatRoom = {
-						params: {
-							chatID: String(chatID),
-							messagesOffset: String(messagesOffset),
-						},
-					};
+					if (rows.length === 0) {
+						return res.send(rows);
+					} else {
+						const chatID = rows[0].chat_id;
+						let desiredChatRoom = {
+							params: {
+								chatID: String(chatID),
+								messagesOffset: String(messagesOffset),
+							},
+						};
 
-					getChatMessages(desiredChatRoom, (response) => {
-						res.send(response);
-					});
+						getChatMessages(desiredChatRoom, (response) => {
+							if (response !== undefined) {
+								res.send(response);
+							} else {
+								logger.error("Messages", {err});
+								return res.status(500).send("internal error");
+							}
+						});
+					}
 				}
 			} catch (err) {
-				console.log(err.message);
+				logger.error("Chats", {err});
+				return res.status(500).send("internal error");
 			}
 		}
 	);
@@ -150,144 +175,151 @@ createUsersChat = (req, res) => {
 		`select chat_id from Chats where (user_A_id = ${userIdA} and user_B_id = ${userIdB}) or (user_A_id = ${userIdB} and user_B_id = ${userIdA})`,
 		(err, rows) => {
 			try {
-				if (rows.length === 0) {
-					// there is no chat of these users --> should create a new chat for them.
-					mySqlConnection.query(
-						`select * from Connections 
-						where (((user_A_id = ${userIdA} and user_B_id = ${userIdB}) or (user_A_id = ${userIdB} and user_B_id = ${userIdA})) 
-						and connected = 1)`,
-						(err, rows) => {
-							try {
-								if (rows.length > 0) {
-									mySqlConnection.query(
-										`insert into Chats (create_date, last_login, user_A_id, user_B_id) values (curdate(), curdate(), ${userIdA}, ${userIdB})`,
-										(err, rows) => {
-											try {
-												if (rows !== undefined && rows.affectedRows >= 1) {
-													mySqlConnection.query(
-														`select chat_id from Chats where user_A_id = ${userIdA} and user_B_id = ${userIdB}`,
-														(err, rows) => {
-															try {
-																chatRooms.insertNewChatRoom(
-																	rows[0].chat_id,
-																	parseInt(userIdA, 10),
-																	parseInt(userIdB, 10)
-																);
-																const currentChatRoom = chatRooms.getChatRoom(
-																	rows[0].chat_id
-																);
-
-																logger.info(
-																	`create a new chat\nchat id = ${rows[0].chat_id} between users: ${userIdA} and ${userIdB}`
-																);
-
-																if (onlineUsers.includesAUser(userIdA)) {
-																	onlineUsers.updateChatRoomOfUser(
-																		userIdA,
-																		currentChatRoom
-																	);
-																	logger.info(
-																		`user id ${userIdA} chat rooms are now:\n${JSON.stringify(
-																			onlineUsers
-																				.getOnlineUser(userIdA)
-																				.getAllUserChatRooms()
-																		)}`
-																	);
-																	// console.log(
-																	// 	`user id ${userIdA} chat rooms:\n${JSON.stringify(
-																	// 		onlineUsers
-																	// 			.getOnlineUser(userIdA)
-																	// 			.getAllUserChatRooms()
-																	// 	)}`
-																	// );
-																}
-
-																if (onlineUsers.includesAUser(userIdB)) {
-																	onlineUsers.updateChatRoomOfUser(
-																		userIdB,
-																		currentChatRoom
-																	);
-																	logger.info(
-																		`user id ${userIdB} chat rooms are now:\n${JSON.stringify(
-																			onlineUsers
-																				.getOnlineUser(userIdB)
-																				.getAllUserChatRooms()
-																		)}`
-																	);
-																	// console.log(
-																	// 	`user id ${userIdB} chat rooms:\n${JSON.stringify(
-																	// 		onlineUsers
-																	// 			.getOnlineUser(userIdB)
-																	// 			.getAllUserChatRooms()
-																	// 	)}`
-																	// );
-																}
-
-																msgToClient = {
-																	msg: `Chat room between users ${userIdA} and ${userIdB} created.`,
-																};
-																return res.send(msgToClient);
-															} catch (err) {
-																throw new Error("create a new chat - ERROR");
-															}
-														}
-													);
-												} else {
-													throw new Error("create a new chat - ERROR");
-												}
-											} catch (err) {
-												logger.error("Chats", {err});
-												return res.status(500).send("internal error");
-											}
-										}
-									);
-								} else {
-									msgToClient = {
-										msg: `Chat not added. There is no mutual connection between users ${userIdA} and ${userIdB}.`,
-									};
-									return res.send(msgToClient);
-								}
-							} catch (err) {
-								logger.error("Chats", {err});
-								return res.status(500).send("internal error");
-							}
-						}
+				if (err || rows === undefined) {
+					throw new Error(
+						"Chats - UPDATE || INSERT to DB - MySQL syntax Error"
 					);
 				} else {
-					const chatID = rows[0].chat_id;
-					mySqlConnection.query(
-						`update Chats set last_login = curdate() where chat_id = ${chatID}`,
-						(err, rows) => {
-							try {
-								if (rows !== undefined && rows.affectedRows >= 1) {
-									logger.info(
-										"im from updating\nchat rooms in DB are:",
-										chatRooms
-									);
-									let desiredChatRoom = {
-										params: {
-											chatID: String(chatID),
-											messagesOffset: String(0),
-										},
-									};
+					if (rows.length === 0) {
+						// there is no chat of these users --> should create a new chat for them.
+						mySqlConnection.query(
+							`select * from Connections 
+							where (((user_A_id = ${userIdA} and user_B_id = ${userIdB}) or (user_A_id = ${userIdB} and user_B_id = ${userIdA})) 
+							and connected = 1)`,
+							(err, rows) => {
+								try {
+									if (err || rows === undefined) {
+										throw new Error(
+											"Chats - UPDATE || INSERT to DB - MySQL syntax Error"
+										);
+									} else {
+										if (rows.length > 0) {
+											mySqlConnection.query(
+												`insert into Chats (create_date, last_login, user_A_id, user_B_id) values (curdate(), curdate(), ${userIdA}, ${userIdB})`,
+												(err, rows) => {
+													try {
+														if (
+															err ||
+															rows === undefined ||
+															rows.affectedRows < 1
+														) {
+															throw new Error(
+																"Chats - UPDATE || INSERT to DB - MySQL syntax Error"
+															);
+														} else {
+															mySqlConnection.query(
+																`select chat_id from Chats where user_A_id = ${userIdA} and user_B_id = ${userIdB}`,
+																(err, rows) => {
+																	try {
+																		chatRooms.insertNewChatRoom(
+																			rows[0].chat_id,
+																			parseInt(userIdA, 10),
+																			parseInt(userIdB, 10)
+																		);
+																		const currentChatRoom =
+																			chatRooms.getChatRoom(rows[0].chat_id);
 
-									getChatMessages(desiredChatRoom, (response) => {
-										if (response !== undefined) {
-											res.send(response);
+																		logger.info(
+																			`create a new chat\nchat id = ${rows[0].chat_id} between users: ${userIdA} and ${userIdB}`
+																		);
+
+																		if (onlineUsers.includesAUser(userIdA)) {
+																			onlineUsers.updateChatRoomOfUser(
+																				userIdA,
+																				currentChatRoom
+																			);
+																			logger.info(
+																				`user id ${userIdA} chat rooms are now:\n${JSON.stringify(
+																					onlineUsers
+																						.getOnlineUser(userIdA)
+																						.getAllUserChatRooms()
+																				)}`
+																			);
+																		}
+
+																		if (onlineUsers.includesAUser(userIdB)) {
+																			onlineUsers.updateChatRoomOfUser(
+																				userIdB,
+																				currentChatRoom
+																			);
+																			logger.info(
+																				`user id ${userIdB} chat rooms are now:\n${JSON.stringify(
+																					onlineUsers
+																						.getOnlineUser(userIdB)
+																						.getAllUserChatRooms()
+																				)}`
+																			);
+																		}
+
+																		msgToClient = {
+																			msg: `Chat room between users ${userIdA} and ${userIdB} created.`,
+																		};
+																		return res.send(msgToClient);
+																	} catch (err) {
+																		throw new Error(
+																			"create a new chat - ERROR"
+																		);
+																	}
+																}
+															);
+														}
+													} catch (err) {
+														logger.error("Chats", {err});
+														return res.status(500).send("internal error");
+													}
+												}
+											);
 										} else {
-											logger.error("Messages", {err});
-											return res.status(500).send("internal error");
+											msgToClient = {
+												msg: `Chat not added. There is no mutual connection between users ${userIdA} and ${userIdB}.`,
+											};
+											return res.send(msgToClient);
 										}
-									});
-								} else {
-									throw new Error("get chat messages - ERROR");
+									}
+								} catch (err) {
+									logger.error("Chats", {err});
+									return res.status(500).send("internal error");
 								}
-							} catch (err) {
-								logger.error("Chats", {err});
-								return res.status(500).send("internal error");
 							}
-						}
-					);
+						);
+					} else {
+						const chatID = rows[0].chat_id;
+						mySqlConnection.query(
+							`update Chats set last_login = curdate() where chat_id = ${chatID}`,
+							(err, rows) => {
+								try {
+									if (err || rows === undefined || rows.affectedRows < 1) {
+										throw new Error(
+											"Chats - UPDATE || INSERT to DB - MySQL syntax Error"
+										);
+									} else {
+										logger.info(
+											"im from updating\nchat rooms in DB are:",
+											chatRooms
+										);
+										let desiredChatRoom = {
+											params: {
+												chatID: String(chatID),
+												messagesOffset: String(0),
+											},
+										};
+
+										getChatMessages(desiredChatRoom, (response) => {
+											if (response !== undefined) {
+												res.send(response);
+											} else {
+												logger.error("Messages", {err});
+												return res.status(500).send("internal error");
+											}
+										});
+									}
+								} catch (err) {
+									logger.error("Chats", {err});
+									return res.status(500).send("internal error");
+								}
+							}
+						);
+					}
 				}
 			} catch (err) {
 				logger.error("Chats", {err});
@@ -307,31 +339,37 @@ deleteUsersChat = (req, res) => {
 					or (user_A_id = ${userIdB} and user_B_id = ${userIdA})`,
 		(err, rows) => {
 			try {
-				if (rows.length === 0) {
-					msgToClient = {
-						msg: `Chat was not exist`,
-					};
-					return res.send(msgToClient);
+				if (err || rows === undefined) {
+					throw new Error(
+						"Chat - DELETE (select the chat to delete failed) MySQL Error"
+					);
 				} else {
-					const chatID = rows[0].chat_id;
-					let desiredChatRoom = {
-						params: {
-							chatID: String(chatID),
-						},
-					};
+					if (rows.length === 0) {
+						msgToClient = {
+							msg: `Chat was not exist`,
+						};
+						return res.send(msgToClient);
+					} else {
+						const chatID = rows[0].chat_id;
+						let desiredChatRoom = {
+							params: {
+								chatID: String(chatID),
+							},
+						};
 
-					deleteChatMessages(desiredChatRoom, (response) => {
-						if (response !== undefined) {
-							res.send(response);
-						} else {
-							logger.error("Messages", {err});
-							return res.status(500).send("internal error");
-						}
-						//res.send(response);
-					});
+						deleteChatMessages(desiredChatRoom, (response) => {
+							if (response !== undefined) {
+								res.send(response);
+							} else {
+								logger.error("Messages - DELETE MySQL Error");
+								return res.status(500).send("internal error");
+							}
+						});
+					}
 				}
 			} catch (err) {
-				console.log(err.message);
+				logger.error("Chats", {err});
+				return res.status(500).send("internal error");
 			}
 		}
 	);
--- BackEnd_Files/api/controllers/messages.js
+++ BackEnd_Files/api/controllers/messages.js
@@ -1,5 +1,5 @@
 const {
-  sendNotificationHelper,
+	sendNotificationHelper,
 } = require("../../utils/notifications/notifications");
 const onlineUsersArray = require("../../utils/users/onlineUsersArray");
 const dbConfig = require("../../config/db_config");
@@ -8,98 +8,115 @@ const mySqlConnection = dbConfig;
 const logger = require("../../utils/logger");
 
 getChatMessages = (req, cb) => {
-  logger.info("This is an info log");
-  const chatID = req.params.chatID;
-  const messagesOffset = req.params.messagesOffset;
+	logger.info("This is an info log");
+	const chatID = req.params.chatID;
+	const messagesOffset = req.params.messagesOffset;
 
-  mySqlConnection.query(
-    `select * from (select * from Messages where chat_id = ${chatID} limit 50 offset ${messagesOffset}) as T1 order by creation_date asc`,
-    (err, rows) => {
-      try {
-        return cb(rows);
-      } catch (err) {
-        console.log(err.message);
-      }
-    }
-  );
+	mySqlConnection.query(
+		`select * from (select * from Messages where chat_id = ${chatID} limit 50 offset ${messagesOffset}) as T1 order by creation_date asc`,
+		(err, rows) => {
+			return cb(rows);
+		}
+	);
 };
 
 createChatMessage = (req, res) => {
-  logger.info("This is an info log");
-  const sender = req.params.useridA;
-  const receiver = req.params.useridB;
-  const content = req.body.content;
-  const senderOnlineUser = onlineUsers.getOnlineUser(sender);
-  const receiverOnlineUser = onlineUsers.getOnlineUser(receiver);
-  const currentChatRoom = onlineUsers
-    .getOnlineUser(senderOnlineUser.user_id)
-    .getAUserChatRoom(receiver)[0];
+	logger.info("This is an info log");
+	const sender = req.params.useridA;
+	const receiver = req.params.useridB;
+	const content = req.body.content;
+	const senderOnlineUser = onlineUsers.getOnlineUser(sender);
+	const receiverOnlineUser = onlineUsers.getOnlineUser(receiver);
 
-  if (senderOnlineUser === undefined || currentChatRoom === undefined) {
-    res.statusCode = 404;
-    res.send("ERROR! Something went wrong! **Post Message Request**");
-  } else {
-    const chatID = currentChatRoom.chat_id;
+	try {
+		if (senderOnlineUser === undefined) {
+			throw new Error(
+				"Messages - POST send a new message. Sender isn't online to WS"
+			);
+		}
 
-    mySqlConnection.query(
-      `insert into Messages (chat_id, creation_date, sender_user_id, receiver_user_id, content) values (${chatID}, current_timestamp(), ${sender}, ${receiver}, "${content}")`,
-      (err, rows) => {
-        try {
-          const titleToSend = "You got a new message";
-          const bodyToSend = "New message from ";
-          //sendNotificationHelper(req, res, titleToSend, bodyToSend);
+		const currentChatRoom = onlineUsers
+			.getOnlineUser(senderOnlineUser.user_id)
+			.getAUserChatRoom(receiver)[0];
 
-          mySqlConnection.query(
-            `select * from Messages where chat_id = ${chatID} order by creation_date desc limit 1`,
-            (err, rows) => {
-              try {
-                if (rows !== undefined && rows.length > 0) {
-                  if (receiverOnlineUser !== undefined) {
-                    // Sender and Receiver are online. Both got the message on WS
-                    senderOnlineUser.websocket.send(JSON.stringify(rows[0]));
-                    receiverOnlineUser.websocket.send(JSON.stringify(rows[0]));
-                  } else {
-                    // Only sender is online and got the message on WS
-                    senderOnlineUser.websocket.send(JSON.stringify(rows[0]));
-                  }
-                  res.send(rows[0]);
-                } else {
-                  console.log(err.message);
-                }
-              } catch (err) {
-                console.log(err.message);
-              }
-            }
-          );
-        } catch (err) {
-          console.log(err.message);
-        }
-      }
-    );
-  }
+		if (currentChatRoom === undefined) {
+			throw new Error(
+				"Messages - POST send a new message. Chat room isn't exist in the DB"
+			);
+		}
+
+		const chatID = currentChatRoom.chat_id;
+
+		mySqlConnection.query(
+			`insert into Messages (chat_id, creation_date, sender_user_id, receiver_user_id, content) values (${chatID}, current_timestamp(), ${sender}, ${receiver}, "${content}")`,
+			(err, rows) => {
+				try {
+					if (err || rows === undefined || rows.affectedRows < 1) {
+						throw new Error("Messages - POST a new message, MySQL Error");
+					} else {
+						const titleToSend = "You got a new message";
+						const bodyToSend = "New message from ";
+						//sendNotificationHelper(req, res, titleToSend, bodyToSend);
+
+						mySqlConnection.query(
+							`select * from Messages where chat_id = ${chatID} order by creation_date desc limit 1`,
+							(err, rows) => {
+								try {
+									if (err || rows === undefined || rows.length === 0) {
+										throw new Error(
+											"Messages - Get the last message that sent failed. MySQL Error"
+										);
+									} else {
+										if (receiverOnlineUser !== undefined) {
+											// Sender and Receiver are online. Both got the message on WS
+											senderOnlineUser.websocket.send(JSON.stringify(rows[0]));
+											receiverOnlineUser.websocket.send(
+												JSON.stringify(rows[0])
+											);
+										} else {
+											// Only sender is online and got the message on WS
+											senderOnlineUser.websocket.send(JSON.stringify(rows[0]));
+										}
+										res.send(rows[0]);
+									}
+								} catch (err) {
+									throw err;
+								}
+							}
+						);
+					}
+				} catch (err) {
+					throw err;
+				}
+			}
+		);
+	} catch (err) {
+		logger.error("Messages - POST MySQL Error", {err});
+		return res.status(500).send("internal error");
+	}
 };
 
 deleteChatMessages = (req, cb) => {
-  logger.info("This is an info log");
-  const chatID = req.params.chatID;
+	logger.info("This is an info log");
+	const chatID = req.params.chatID;
 
-  mySqlConnection.query(
-    `delete from Messages where chat_id = ${chatID}`,
-    (err, rows) => {
-      try {
-        msgToClient = {
-          msg: `Chat room number ${chatID} is empty`,
-        };
-        return cb(msgToClient);
-      } catch (err) {
-        console.log(err.message);
-      }
-    }
-  );
+	mySqlConnection.query(
+		`delete from Messages where chat_id = ${chatID}`,
+		(err, rows) => {
+			if (err || rows === undefined) {
+				return cb(rows);
+			} else {
+				msgToClient = {
+					msg: `Chat room number ${chatID} is empty`,
+				};
+				return cb(msgToClient);
+			}
+		}
+	);
 };
 
 module.exports = {
-  getChatMessages,
-  createChatMessage,
-  deleteChatMessages,
+	getChatMessages,
+	createChatMessage,
+	deleteChatMessages,
 };
