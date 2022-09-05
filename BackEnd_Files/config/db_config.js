6c12e5742b33003b4fa7bc027ce9278be9dd4913
shellysp94 <59768741+shellysp94@users.noreply.github.com>
2022-09-05 13:41:03 +0300

    .env config

--- BackEnd_Files/config/db_config.js
+++ BackEnd_Files/config/db_config.js
@@ -1,24 +1,19 @@
-const mysql=require('mysql');
-require('dotenv').config({path:__dirname+'/../.env'})
-
+const mysql = require("mysql");
+require("dotenv").config({ path: __dirname + "/../lets.env" });
 
 const mySqlConnection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASS,
-    database: process.env.MYSQL_NAME,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_NAME,
 });
 
-mySqlConnection.connect((err)=>
-{
-    if(err)
-    {
-        console.log("Error:" + err.message);
-    }
-    else
-    {
-        console.log("connected");
-    }
-})
+mySqlConnection.connect((err) => {
+  if (err) {
+    console.log("Error:" + err.message);
+  } else {
+    console.log("connected");
+  }
+});
 
 module.exports = mySqlConnection;
