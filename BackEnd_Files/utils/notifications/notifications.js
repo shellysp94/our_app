76d2f569ebd90b594f99230d6176f30487893ffa
shellysp94 <59768741+shellysp94@users.noreply.github.com>
2022-09-06 12:01:25 +0300

    update table name in utils notifications

--- BackEnd_Files/utils/notifications/notifications.js
+++ BackEnd_Files/utils/notifications/notifications.js
@@ -1,62 +1,62 @@
 const dbConfig = require("../../config/db_config");
 const mySqlConnection = dbConfig;
 const {
-	getUserConfigurationInner,
+  getUserConfigurationInner,
 } = require("../../api/controllers/userConfiguration");
-const {sendNotification} = require("../../api/fcm");
-const {admin} = require("../../config/firebase_config");
+const { sendNotification } = require("../../api/fcm");
+const { admin } = require("../../config/firebase_config");
 
 const sendNotificationHelper = (req, res, titleToSend, bodyToSend) => {
-	mySqlConnection.query(
-		`SELECT b.*, a.device_token
-                from device_token a
+  mySqlConnection.query(
+    `SELECT b.*, a.device_token
+                from Device_token a
                 right join user_configuration b
                 on a.user_id = b.user_id
                 where a.user_id = ? or b.user_id = ?`,
-		[req.params.useridB, req.params.useridA],
-		(newErr, newRows) => {
-			try {
-				if (newRows.length > 0) {
-					valuesForNotification = extractParametersForNotification(
-						req,
-						newRows,
-						titleToSend,
-						bodyToSend
-					);
-					sendNotification(
-						res,
-						valuesForNotification.deviceTokenToSend,
-						valuesForNotification.titleToSend,
-						valuesForNotification.bodyToSend,
-						valuesForNotification.userIdTosend
-					);
-				}
-			} catch (newErr) {
-				console.log(newErr);
-			}
-		}
-	);
+    [req.params.useridB, req.params.useridA],
+    (newErr, newRows) => {
+      try {
+        if (newRows.length > 0) {
+          valuesForNotification = extractParametersForNotification(
+            req,
+            newRows,
+            titleToSend,
+            bodyToSend
+          );
+          sendNotification(
+            res,
+            valuesForNotification.deviceTokenToSend,
+            valuesForNotification.titleToSend,
+            valuesForNotification.bodyToSend,
+            valuesForNotification.userIdTosend
+          );
+        }
+      } catch (newErr) {
+        console.log(newErr);
+      }
+    }
+  );
 };
 
 const extractParametersForNotification = (req, newRows, title, body) => {
-	let tokensArr = [];
-	for (var i = 0; i < newRows.length; i++) {
-		if (newRows[i].user_id == req.params.useridB) {
-			tokensArr.push(newRows[i].device_token);
-		}
+  let tokensArr = [];
+  for (var i = 0; i < newRows.length; i++) {
+    if (newRows[i].user_id == req.params.useridB) {
+      tokensArr.push(newRows[i].device_token);
+    }
 
-		if (newRows[i].user_id == req.params.useridA) {
-			var senderName = newRows[i].first_name + " " + newRows[i].last_name;
-		}
-	}
-	const paramsForNotification = {
-		deviceTokenToSend: tokensArr,
-		titleToSend: title,
-		//bodyToSend: body + `${senderName}`,
-		bodyToSend: body.concat(senderName),
-		userIdTosend: req.params.useridB,
-	};
-	return paramsForNotification;
+    if (newRows[i].user_id == req.params.useridA) {
+      var senderName = newRows[i].first_name + " " + newRows[i].last_name;
+    }
+  }
+  const paramsForNotification = {
+    deviceTokenToSend: tokensArr,
+    titleToSend: title,
+    //bodyToSend: body + `${senderName}`,
+    bodyToSend: body.concat(senderName),
+    userIdTosend: req.params.useridB,
+  };
+  return paramsForNotification;
 };
 
-module.exports = {extractParametersForNotification, sendNotificationHelper};
+module.exports = { extractParametersForNotification, sendNotificationHelper };
