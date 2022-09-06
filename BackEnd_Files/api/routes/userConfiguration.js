663d6ffdb4fe9ab47adf0237bee175ae6165903a
MorZemach <90982428+MorZemach@users.noreply.github.com>
2022-09-06 17:04:44 +0300

    user configuration QA

--- BackEnd_Files/api/routes/userConfiguration.js
+++ BackEnd_Files/api/routes/userConfiguration.js
@@ -3,20 +3,21 @@ const {verifyToken} = require("../controllers/auth");
 const router = express.Router();
 
 const {
-    getAllUsersConfiguration,
-    getUserConfiguration,
-    getUsersConfigurationByRadius, 
-    createUserConfiguration,
-    deleteUserConfiguration,
-    updateUserConfiguration
-} = require('../controllers/userConfiguration');
+	getUserConfiguration,
+	createUserConfiguration,
+	deleteUserConfiguration,
+	updateUserConfiguration,
+} = require("../controllers/userConfiguration");
 
-router.get('/:curr_userid/:userid',verifyToken,getUserConfiguration);
-router.get('/:userid',verifyToken,getUserConfiguration); 
-router.get('/',getAllUsersConfiguration);
-router.get('/location/:user_id/:radius',verifyToken,getUsersConfigurationByRadius);
-router.post('/:userid',verifyToken, createUserConfiguration); 
-router.delete('/:userid',verifyToken, deleteUserConfiguration);
-router.put('/:userid',verifyToken, updateUserConfiguration);
+router.get("/:curr_userid/:userid", getUserConfiguration);
+router.get("/:userid", getUserConfiguration);
+// router.get(
+// 	"/location/:user_id/:radius",
+// 	verifyToken,
+// 	getUsersConfigurationByRadius
+// );
+router.post("/:userid", verifyToken, createUserConfiguration);
+router.delete("/:userid", verifyToken, deleteUserConfiguration);
+router.put("/:userid", verifyToken, updateUserConfiguration);
 
 module.exports = router;
