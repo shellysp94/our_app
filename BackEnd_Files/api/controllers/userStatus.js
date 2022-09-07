7a630bc105b27ff30d60a97b3ddb5817fe5164a4
shellysp94 <59768741+shellysp94@users.noreply.github.com>
2022-09-07 20:11:21 +0300

    user error

--- BackEnd_Files/api/controllers/userStatus.js
+++ BackEnd_Files/api/controllers/userStatus.js
@@ -10,10 +10,10 @@ getUserStatus = (req, res) => {
     `select * from user_status where user_id = ${userid}`,
     (err, rows) => {
       try {
-        if (err || rows === undefined || rows.length === 0) {
+        if (err || rows === undefined) {
           throw new Error("User Status - GET request MySQL Error");
         } else {
-          res.send(rows[0]);
+          res.send(rows);
         }
       } catch (err) {
         errLogger.error("User Status - GET MySQL Error", { err });
