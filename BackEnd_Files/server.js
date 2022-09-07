23d889f3eec5c19ffb488b80e7bb461c0b37cce3
MorZemach <90982428+MorZemach@users.noreply.github.com>
2022-09-07 15:12:10 +0300

    server - QA and logger

--- BackEnd_Files/server.js
+++ BackEnd_Files/server.js
@@ -1,12 +1,12 @@
+const {updateLocation} = require("./utils/websocket/locationWebSocket");
+const {closingWebSocket} = require("./utils/websocket/closeWebSocket");
 const onlineUsersArray = require("./utils/users/onlineUsersArray");
+const {infoLogger, errLogger} = require("./utils/logger");
 const publicToken = require("./config/auth_config");
 const dbConfig = require("./config/db_config");
 const {app, jwt} = require("./app");
 const WebSocket = require("ws");
 const http = require("http");
-// const url = require("url");
-const {closingWebSocket} = require("./utils/websocket/closeWebSocket");
-const {updateLocation} = require("./utils/websocket/locationWebSocket");
 const port = process.env.PORT || 3000;
 const mySqlConnection = dbConfig;
 const server = http.createServer(app);
@@ -26,46 +26,42 @@ const myQuery = (query) => {
 
 wss.on("connection", async (ws, req) => {
 	try {
-		//let token = url.parse(req.url, true).query.token;
 		const token = req.headers.authorization.split(" ")[1];
 		let userid;
 		const rows = await myQuery(
 			`select user_id from Users where token = "${token}"`
 		);
+
 		if (rows.length > 0) {
 			userid = rows[0].user_id;
-			console.log("user id:", userid);
 			jwt.verify(token, publicToken, async (err, payload) => {
 				if (err) {
-					console.log(`Error: ${err.message}. connection closed`);
+					errLogger.error(`Error: ${err.message}. connection closed`);
+					ws.send(`Error: ${err.message}. connection closed`);
 					ws.close();
 				} else {
 					await onlineUsers.insertNewOnlineUser(userid, ws);
-					console.log("--------------------------------------------");
-					console.log("Online users array is now:\n");
-					console.log("--------------------------------------------");
-					console.log(onlineUsers);
+					infoLogger.info("online users array:");
+					infoLogger.info(onlineUsers);
 				}
 			});
 		} else {
-			console.log("Token illegal. connection closed");
+			errLogger.error("Token Illegal. Connection Closed");
+			ws.send("Token illegal. connection closed");
 			ws.close();
 		}
-	} catch (e) {
-		console.log(e.message);
+	} catch (err) {
+		errLogger.error(`Server - connection to WS, error\n${err}`);
+		ws.send(500);
+		ws.close();
 	}
 
 	ws.on("message", (message) => {
-		//console.log(`Received message => ${message} `);
 		updateLocation(onlineUsers, message, ws);
 	});
 
 	ws.on("close", () => {
 		closingWebSocket(onlineUsers, ws);
-		// console.log(
-		// 	"online users array from server close ws:",
-		// 	JSON.stringify(onlineUsers)
-		// );
 	});
 });
 
--- BackEnd_Files/utils/users/onlineUsersArray.js
+++ BackEnd_Files/utils/users/onlineUsersArray.js
@@ -1,108 +1,108 @@
 const onlineUser = require("./onlineUser");
-const { infoLogger } = require("../logger");
+const {infoLogger} = require("../logger");
 
 let onlineUsersArray = class {
-  constructor() {
-    this.onlineUsers = [];
-    infoLogger.info("Online users array C'tor:");
-  }
+	constructor() {
+		this.onlineUsers = [];
+		infoLogger.info("Online users array C'tor:");
+	}
 
-  async insertNewOnlineUser(user_id, websocket) {
-    let isExists = 0;
-    this.onlineUsers.forEach((onlineUser) => {
-      if (parseInt(onlineUser.getUserId()) === parseInt(user_id)) {
-        isExists = 1;
-      }
-    });
+	async insertNewOnlineUser(user_id, websocket) {
+		let isExists = 0;
+		this.onlineUsers.forEach((onlineUser) => {
+			if (parseInt(onlineUser.getUserId()) === parseInt(user_id)) {
+				isExists = 1;
+			}
+		});
 
-    if (isExists === 0) {
-      const newOnlineUser = new onlineUser();
-      await newOnlineUser.initOnlineUser(user_id, websocket);
-      this.onlineUsers.push(newOnlineUser);
-      infoLogger.info(
-        `Insert a new online user-\nInserting user_id = ${user_id} and his WS to the array!`
-      );
-    } else {
-      infoLogger.info(
-        `Insert a new online user-\nUser id ${user_id} is already in the array. Didn't insert him again.`
-      );
-    }
-  }
+		if (isExists === 0) {
+			const newOnlineUser = new onlineUser();
+			await newOnlineUser.initOnlineUser(user_id, websocket);
+			this.onlineUsers.push(newOnlineUser);
+			infoLogger.info(
+				`Insert a new online user - Inserting user_id = ${user_id} and his WS to the array!`
+			);
+		} else {
+			infoLogger.info(
+				`Insert a new online user - User id ${user_id} is already in the array. Didn't insert him again.`
+			);
+		}
+	}
 
-  removeAnOnlineUser(user_id) {
-    this.onlineUsers.forEach((onlineUser) => {
-      if (onlineUser.getUserId() === user_id) {
-        infoLogger.info(
-          "The ready state of ws:",
-          onlineUser.websocket.readyState
-        );
-        if (
-          onlineUser.websocket.readyState === 2 ||
-          onlineUser.websocket.readyState === 3
-        ) {
-          this.onlineUsers.splice(this.onlineUsers.indexOf(onlineUser), 1);
-          infoLogger.info("Remove from online users array");
-        }
-      }
-    });
-  }
+	removeAnOnlineUser(user_id) {
+		this.onlineUsers.forEach((onlineUser) => {
+			if (onlineUser.getUserId() === user_id) {
+				infoLogger.info(
+					"The ready state of ws:",
+					onlineUser.websocket.readyState
+				);
+				if (
+					onlineUser.websocket.readyState === 2 ||
+					onlineUser.websocket.readyState === 3
+				) {
+					this.onlineUsers.splice(this.onlineUsers.indexOf(onlineUser), 1);
+					infoLogger.info("Remove from online users array");
+				}
+			}
+		});
+	}
 
-  getOnlineUsersArray() {
-    return this.onlineUsers;
-  }
+	getOnlineUsersArray() {
+		return this.onlineUsers;
+	}
 
-  getOnlineUser(user_id) {
-    let onlineUser = this.onlineUsers.find(
-      (user) => parseInt(user.user_id) === parseInt(user_id)
-    );
+	getOnlineUser(user_id) {
+		let onlineUser = this.onlineUsers.find(
+			(user) => parseInt(user.user_id) === parseInt(user_id)
+		);
 
-    return onlineUser;
-  }
+		return onlineUser;
+	}
 
-  includesAUser(user_id) {
-    let isExists = 0;
-    this.onlineUsers.forEach((onlineUser) => {
-      if (parseInt(onlineUser.user_id) === parseInt(user_id)) {
-        isExists = 1;
-      }
-    });
+	includesAUser(user_id) {
+		let isExists = 0;
+		this.onlineUsers.forEach((onlineUser) => {
+			if (parseInt(onlineUser.user_id) === parseInt(user_id)) {
+				isExists = 1;
+			}
+		});
 
-    return isExists;
-  }
+		return isExists;
+	}
 
-  async updateChatRoomOfUser(user_id, chatRoom) {
-    const onlineUser = this.getOnlineUser(user_id);
-    await onlineUser.updateOnlineUserChatRoomsArray(chatRoom);
+	async updateChatRoomOfUser(user_id, chatRoom) {
+		const onlineUser = this.getOnlineUser(user_id);
+		await onlineUser.updateOnlineUserChatRoomsArray(chatRoom);
 
-    infoLogger.info(
-      `---FROM ONLINE USERS **ARRAY**---\n user's number: ${user_id} chat rooms array is now:\n${this.getOnlineUser(
-        user_id
-      ).getAllUserChatRooms()}`
-    );
-  }
+		infoLogger.info(
+			`---FROM ONLINE USERS **ARRAY**--- user's number: ${user_id} chat rooms array is now:\n${this.getOnlineUser(
+				user_id
+			).getAllUserChatRooms()}`
+		);
+	}
 
-  getUserIdAccordingToWebSocket(ws) {
-    let result = -1;
-    this.onlineUsers.forEach((onlineUser) => {
-      if (onlineUser.websocket === ws) {
-        result = onlineUser.user_id;
-      }
-    });
+	getUserIdAccordingToWebSocket(ws) {
+		let result = -1;
+		this.onlineUsers.forEach((onlineUser) => {
+			if (onlineUser.websocket === ws) {
+				result = onlineUser.user_id;
+			}
+		});
 
-    return result;
-  }
+		return result;
+	}
 };
 
 class Singleton {
-  constructor() {
-    if (!Singleton.instance) {
-      Singleton.instance = new onlineUsersArray();
-    }
-  }
+	constructor() {
+		if (!Singleton.instance) {
+			Singleton.instance = new onlineUsersArray();
+		}
+	}
 
-  getInstance() {
-    return Singleton.instance;
-  }
+	getInstance() {
+		return Singleton.instance;
+	}
 }
 
 module.exports = Singleton;
