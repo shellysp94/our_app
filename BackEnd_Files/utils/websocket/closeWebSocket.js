6e6b78cd157831c96e39aa467a4de964490ba9cf
shellysp94 <59768741+shellysp94@users.noreply.github.com>
2022-09-07 19:34:06 +0300

    info logger

--- BackEnd_Files/utils/websocket/closeWebSocket.js
+++ BackEnd_Files/utils/websocket/closeWebSocket.js
@@ -1,15 +1,15 @@
-const {info} = require("winston");
-const {infoLogger} = require("../logger");
+const { info } = require("winston");
+const { infoLogger } = require("../logger");
 
 function closingWebSocket(onlineUsers, ws) {
-	infoLogger.info(`Closing Websocket`);
-	onlineUsers.getOnlineUsersArray().forEach((user) => {
-		if (user.websocket === ws) {
-			info.infoLogger(`User ${user.user_id} Websocket Closed`);
-			onlineUsers.removeAnOnlineUser(user.user_id);
-			ws.send(`User Disconnected`);
-		}
-	});
+  infoLogger.info(`Closing Websocket`);
+  onlineUsers.getOnlineUsersArray().forEach((user) => {
+    if (user.websocket === ws) {
+      infoLogger.info(`User ${user.user_id} Websocket Closed`);
+      onlineUsers.removeAnOnlineUser(user.user_id);
+      ws.send(`User Disconnected`);
+    }
+  });
 }
 
-module.exports = {closingWebSocket: closingWebSocket};
+module.exports = { closingWebSocket: closingWebSocket };
