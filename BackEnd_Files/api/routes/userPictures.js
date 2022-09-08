96ac25b2e0bb1ad69bbea83a9c4598f202b6fb61
shellysp94 <59768741+shellysp94@users.noreply.github.com>
2022-09-08 15:18:08 +0300

    user pic token

--- BackEnd_Files/api/routes/userPictures.js
+++ BackEnd_Files/api/routes/userPictures.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {verifyToken} = require("../controllers/auth");
+const { verifyToken } = require("../controllers/auth");
 
 const {
-    getUserPictures, 
-    deleteUserPicture,
-    //updateUserPicture,
-    getUserMainPicture,
-    uploadBase64Image
-} = require('../controllers/userPictures');
+  getUserPictures,
+  deleteUserPicture,
+  //updateUserPicture,
+  getUserMainPicture,
+  uploadBase64Image,
+} = require("../controllers/userPictures");
 
-router.get('/:userid',verifyToken,getUserPictures); //get user by appid (/hobbies/:userid)
-router.get('/main/:userid',verifyToken, getUserMainPicture);
-router.post('/:userid',verifyToken, uploadBase64Image);
-router.delete('/:userid',verifyToken, deleteUserPicture) //delete user specific picture
+router.get("/:userid", getUserPictures); //get user by appid (/hobbies/:userid)
+router.get("/main/:userid", verifyToken, getUserMainPicture);
+router.post("/:userid", verifyToken, uploadBase64Image);
+router.delete("/:userid", verifyToken, deleteUserPicture); //delete user specific picture
 //router.put('/:userid',upload, updateUserPicture); //update user
 
-module.exports=router;
+module.exports = router;
